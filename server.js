da897ea4b510e9329b6359b9436210259fbbe5c5
Roman Makhov <nekromantos30888666@gmail.com>
2023-06-20 08:11:18 +0300

    fix: PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const inexPage = fs.readFileSync(path.resolve(__dirname, 'dist', 'index.html'));
 
 app.use(stat('dist'));
